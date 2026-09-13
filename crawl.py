d0a1b1261bb0f3591ad5537675b0c035e3936049
Volodymyr <dodik55vovik@gmail.com>
2026-09-13 18:17:09 -0400

    created a recursive function that not only crawls a page but also pages that are linked on the main page. Also changed main.py to use the new function.

--- crawl.py
+++ crawl.py
@@ -1,11 +1,11 @@
 from urllib.parse import SplitResult, urlsplit, urljoin
 from bs4 import BeautifulSoup
-
+import requests
 def normalize_url(url:str) -> str:
     url_split: SplitResult = urlsplit(url)
     normal_url: str = f"{url_split.netloc.lower()}{url_split.path}"
     normal_url: str = normal_url.rstrip('/')
-    return normal_url
+    return normal_url.lower()
 
 def get_heading_from_html(html: str) -> str:
     soup: BeautifulSoup = BeautifulSoup(html, "html.parser")
@@ -38,8 +38,10 @@ def get_urls_from_html(html: str, base_url: str) -> list[str]:
     links_list: list[str] = []
 
     for link in links:
-        links_list.append(urljoin(base_url, link.get("href")))
-
+        try:
+            links_list.append(urljoin(base_url, link.get("href")))
+        except Exception as e:
+            print(f"{str(e)}: {link.get("href")}")
     return links_list
 
 def get_images_from_html(html: str, base_url: str) -> list[str]:
@@ -60,6 +62,60 @@ def extract_page_data(html: str, page_url: str):
     page_data["outgoing_links"] = get_urls_from_html(html, page_url)
     page_data["image_urls"] = get_images_from_html(html, page_url)
     return page_data
+
+def get_html(url):
+    try:
+        response = requests.get(url, headers={"User-Agent": "BootCrawler/1.0"})
+    except Exception as e:
+        raise Exception(f"Error: {e}")
+    if response.status_code >= 400:
+        raise Exception("error communication with the server")
+    elif "text/html" not in response.headers["content-type"]:
+        raise Exception("content is not a html or text")
+
+    return response.text
+
+def crawl_page(base_url, 
+               current_url: str | None = None, 
+               page_data: dict[dict, str] | None = None):
+
+    if current_url is None:
+        current_url = base_url
+
+    if page_data is None:
+        page_data = {}
+
+    if urlsplit(base_url).netloc.lower() != urlsplit(current_url).netloc.lower():
+        return page_data
+
+    normal_url = normalize_url(current_url)
+
+    if normal_url in page_data.keys():
+        return page_data
+
+    try:
+        html = get_html(current_url)
+    except Exception as e:
+        print(f"Error: {e}")
+
+    data = extract_page_data(html, current_url)
+
+    page_data[normal_url] = data
+
+    for url in page_data[normal_url]["outgoing_links"]:
+        page_data = crawl_page(base_url, url, page_data)
+
+    return page_data
+
+
+
+
+
+
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+from urllib.parse import urlsplit, urljoin
+import sys
+from crawl import crawl_page
+
 def main():
-    print("Hello from webcrawler!")
+
+    if len(sys.argv) < 2:
+        print("no website provided")
+        sys.exit(1)
+    elif len(sys.argv) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+    else:
+        print(f"starting crawl of: {sys.argv[1]}")
+
+    try:
+        result = crawl_page(sys.argv[1])
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        sys.exit(1)
+
+    print(result)
+
 
 
 if __name__ == "__main__":
